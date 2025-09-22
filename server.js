e07eed7df75a5a7bf0e8bb6e7e8f2ed83ecee1f1
moroninavarro <moronialanis23@gmail.com>
2025-09-22 12:44:06 -0700

    change

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ const utilities = require("./utilities/")
 /* ***********************
  * View Engine and Templates
  *************************/
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
