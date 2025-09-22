1b151964e3279c4915479ef27e03c09e02ea27c0
moroninavarro <moronialanis23@gmail.com>
2025-09-22 12:57:11 -0700

    change

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ const utilities = require("./utilities/")
   saveUninitialized: true,
   name: 'sessionId',
 }))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
