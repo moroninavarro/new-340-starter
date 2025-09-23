77cdd3653d3b9b1ff2e5f718d63655518852ab77
moroninavarro <moronialanis23@gmail.com>
2025-09-22 18:23:21 -0700

    change

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,5 +3,5 @@ const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
-
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 module.exports = router;
